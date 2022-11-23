9d27d2d1a21fde35a7b540b4adb767f5c0904a1b
thiagosauro <thsilva.developer@gmail.com>
2022-11-23 00:07:29 -0300

    Primeiro commit. O banco de dados está pronto e parte do axios também.

---
+++ server.js
@@ -0,0 +1,13 @@
+//server
+
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+  console.log('Servidor JSON está operando!')
+})
